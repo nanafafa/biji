7bac060733c07bc97ea1804476ee52fa96f88b38
nanafafa <473066712@qq.com>
2019-11-10 15:19:48 +0800

    11

--- src/main/java/com/riji/controller/SwaggerController.java
+++ src/main/java/com/riji/controller/SwaggerController.java
@@ -3,11 +3,14 @@ package com.riji.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ *
+ * http://localhost:8080/swagger-ui.html#/
  * @author 杨鹏发
  * @date 2019/11/9 0009
  */
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SwaggerController {
     @ApiOperation("艺人档期列表")
-    @RequestMapping("/show")
+    @GetMapping("/show")
     public String swaggerIndex(@RequestParam("page") String page,
                                @RequestParam("pageSize") String pageSize,
                                @RequestParam("condition") String condition){
@@ -23,7 +26,7 @@ public class SwaggerController {
     }
     @ApiOperation("获取会员的相关信息")
     @ApiImplicitParam(name="userName",value = "用户信息参数",required = true,dataType = "String")
-    @RequestMapping("/getMember")
+    @GetMapping("/getMember")
     public String getMember(String userName){
         return "username";
     }
