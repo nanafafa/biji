931fb575fc191fb94ac6dce7a1386224dddfb3c3
root <473066712@qq.com>
2021-04-06 13:36:32 +0800

    es

--- src/test/java/com/riji/fafaya/FafayaApplicationTests.java
+++ src/test/java/com/riji/fafaya/FafayaApplicationTests.java
@@ -3,10 +3,19 @@ package com.riji.fafaya;
 import com.alibaba.fastjson.JSON;
 import com.riji.es.config.pojo.User;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
@@ -14,13 +23,22 @@ import org.elasticsearch.client.indices.CreateIndexResponse;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.naming.directory.SearchResult;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -84,6 +102,87 @@ public class FafayaApplicationTests {
         System.out.println(exists);
     }
     //获取文档的信息
+    @Test
+    public void testGetDocument() throws IOException {
+        GetRequest request = new GetRequest("kuang_index", "1");
+        GetResponse getResponse = restHighLevelClient.get(request, RequestOptions.DEFAULT);
+        System.out.println(getResponse.getSourceAsString());//打印文档的内容
+        System.out.println(getResponse);//返回全部内容
+    }
+    //更新文档的信息
+    @Test
+    public void testUpdateDocument() throws IOException {
+        UpdateRequest request = new UpdateRequest("kuang_index", "1");
+        request.timeout("1s");
+        User user = new User("狂神说", 18);
+        request.doc(JSON.toJSONString(user),XContentType.JSON);
+
+        UpdateResponse update = restHighLevelClient.update(request, RequestOptions.DEFAULT);
+        System.out.println(update.status());//打印更改状态
+    }
+
+    //删除文档的信息
+    @Test
+    public void testDeleteDocument() throws IOException {
+        DeleteRequest request = new DeleteRequest("kuang_index", "3");
+        request.timeout("1s");
+        DeleteResponse deleteResponse = restHighLevelClient.delete(request, RequestOptions.DEFAULT);
+        System.out.println(deleteResponse.status());
+    }
+
+
+    //真的项目一般都会批量插入数据
+    @Test
+    public void testBulkRequest() throws IOException {
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.timeout("10s");
+        List<User> list = new ArrayList<>();
+        list.add(new User("kuangshen1",3));
+        list.add(new User("kuangshen2",3));
+        list.add(new User("kuangshen3",3));
+        list.add(new User("qianjiang1",3));
+        list.add(new User("qianjiang2",3));
+        list.add(new User("qianjiang3",3));
+        //批处理请求
+        for (int i = 0; i < list.size(); i++) {
+            bulkRequest.add(new IndexRequest("kuang_index").id(""+(i+1)).source(JSON.toJSONString(list.get(i)),XContentType.JSON));
+        }
+        BulkResponse bulkResponse = restHighLevelClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(bulkResponse.hasFailures());//是否执行失败  false代表成功
+
+
+    }
+    //查询带条件的
+
+    /**
+     * SearchRequest:搜索请求
+     * SearchSourceBuilder：条件构造
+     * HighlightBuilder构建高亮
+     * TermQueryBuilder：构建精确查询
+     * MatchAllQueryBuilder 构建全部的查询
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testSearch() throws IOException {
+        SearchRequest searchRequest = new SearchRequest("kuang_index");
+        //构建搜索条件
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //查询条件，我们可以使用QueryBuilders 工具来实现
+        //QueryBuilders.termQuery 精确
+        //QueryBuilders.matchAllQuery()  匹配所有
+        TermQueryBuilder termQueryBuilder = QueryBuilders.termQuery("name", "kuangshen1");
+        searchSourceBuilder.query(termQueryBuilder);
+        searchSourceBuilder.timeout(new TimeValue(60, TimeUnit.SECONDS));
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHits hits = searchResponse.getHits();
+        System.out.println(JSON.toJSONString(hits));
+        System.out.println("=====================");
+        for (SearchHit documentFields : searchResponse.getHits().getHits()) {
+            System.out.println(documentFields.getSourceAsMap());
+        }
+    }
 
 
     @Test
