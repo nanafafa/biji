729b423fcc6ba0104489eb6e90a5aa05ec2e3977
root <473066712@qq.com>
2021-07-18 19:56:11 +0800

    api安全

---
+++ src/main/java/com/riji/controller/apisecticy/TokenInfo.java
@@ -0,0 +1,16 @@
+package com.riji.controller.apisecticy;
+
+
+import lombok.Data;
+
+@Data
+public class TokenInfo {
+    /** token类型: api:0 、user:1 */
+    private Integer tokenType;
+
+    /** App 信息 */
+    private AppInfo appInfo;
+
+    /** 用户其他数据 */
+    private UserInfo userInfo;
+}
