3ffb0fdcf43b73a26d7062b7b04e19b027b9bb63
root <473066712@qq.com>
2021-08-01 21:11:55 +0800

    接口安全

--- src/main/java/com/riji/controller/apisecticy/ApiTokenController.java
+++ src/main/java/com/riji/controller/apisecticy/ApiTokenController.java
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 @RestController
 @RequestMapping("/api/token")
-public class TokenController {
+public class ApiTokenController {
 
     @Autowired
     private RedisTemplate redisTemplate;
